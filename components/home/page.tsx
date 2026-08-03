09f7eb8ad049cb636398752f009e8be11a98f085
gulshan kumar <gulshanneya515@gmail.com>
2026-08-03 17:52:07 +0530

    commits

--- components/home/page.tsx
+++ components/home/page.tsx
@@ -1,14 +1,14 @@
 import Hero from "@/components/home/Hero";
 import Stats from "@/components/home/Stats";
-import Services from "@/components/home/Services";
+import ServicePreview from "@/components/home/ServicePreview";
 import Providers from "@/components/home/Providers";
 
 export default function Home() {
   return (
-    <>
+    < >
       <Hero />
       <Stats />
-      <Services />
+      <ServicePreview />
       <Providers />
     </>
   );
